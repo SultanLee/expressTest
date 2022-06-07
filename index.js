d07553a7ee63a029cf0c4a5cab553c1ff7320e20
이승환 <dyrhldnjcl11@gmail.com>
2022-06-08 01:49:16 +0900

    <feat> add login, logout, auth

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const moongoose = require('mongoose')
 const {User} = require("./models/User")
 const bodyParser = require('body-parser')
 const config = require('./config/dev')
+const cookieParser = require("cookie-parser")
+const {auth} = require('./middleware/auth')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
+//mongoDB connect
 moongoose.connect(config.mongoURI)
     .then(()=> console.log('MongoDB Connected'))
     .catch(err=>console.log(err))
 
-app.get('/', (req, res) => res.send('Hellow World'))
-app.post('/register', (req, res) =>{
+//user register
+app.post('/api/users/register', (req, res) =>{
     const user = new User(req.body)
     user.save((err,userInfo)=>{
         if(err) return res.json({success: false, err})
@@ -24,4 +28,55 @@ app.post('/register', (req, res) =>{
     })
 })
 
-app.listen(port,() => console.log(`Example App listening on port ${port}!`))
+//user login
+app.post('/api/users/login', (req, res) =>{
+    User.findOne({email: req.body.email}, (err,user) =>{
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "email not found."
+            })
+        }
+        user.comparePassword(req.body.password, (err, isMatch) =>{
+            if(!isMatch)
+                return res.json({
+                    loginSuccess: false,
+                    message: "password is wrong."
+                })
+            user.makeToken((err,user) =>{
+                if(err) return res.status(400).send(err);
+                res.cookie("auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true,
+                        userId: user._id
+                    })
+            })
+        })
+    })
+})
+
+//user auth
+app.get('/api/users/auth', auth, (req, res) =>{
+    res.status(200).json({
+        _id: req.user._id,
+        email: req.user.email,
+        name: req.user.name
+    })
+})
+
+//user logout
+app.get('/api/users/logout', auth, (req, res) =>{
+    User.findOneAndUpdate({_id: req.user._id},
+        {token:""},
+        (err,user) => {
+            if(err) return res.json({
+                success : false, err
+            })
+            return res.status(200).send({
+                success:true
+            })
+        })
+})
+
+app.listen(port,() => console.log(`Port : ${port}`))
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -19,6 +23,50 @@ const userSchema = mongoose.Schema({
         type:Number
     }
 })
+
+//hash
+userSchema.pre('save', function (next){
+    var user = this;
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            });
+        });
+    } else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, callBack){
+    bcrypt.compare(plainPassword, this.password, function(err,isMatch){
+        if(err) return callBack(err)
+        callBack(null, isMatch)
+    })
+}
+
+userSchema.methods.makeToken = function(callBack){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'sToken')
+    user.token = token
+    user.save(function(err,user){
+        if(err) return callBack(err)
+        callBack(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, callBack) {
+    var user = this
+    jwt.verify(token, 'sToken', function(err, decoded){
+        user.findOne({"_id": decoded, "token" : token}, function (err, user){
+            if(err) return callBack(err)
+            callBack(null,user)
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
